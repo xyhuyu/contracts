5ce369e5eeb18c1e94d17780791a53dec3f377b4
李兴 <289373728@qq.com>
2019-03-05 18:47:53 +0800

    增加游戏管理和用户管理

--- contracts/Ymchain.sol
+++ contracts/Ymchain.sol
@@ -10,11 +10,49 @@ contract Ymchain is ERC20, ERC20Detailed, Ownable {
     uint8 private _decimals = 18;
     uint256 private _totalSupply = 2000000000 * (10 ** uint256(_decimals));
 
-
+	//代币初始化
     constructor(address _owner) Ownable(_owner) public ERC20Detailed(_name, _symbol, _decimals) {
 		_mint(_owner, _totalSupply);
 
     }
+
+    //游戏管理
+	event eventSetGame(uint _gameId, string name);
+
+ 	struct Game {
+		string name;
+	}
+	mapping(uint => Game) public _games;
+
+	function setGame(uint gameid, string memory name) onlyOwner public {
+        _games[gameid].name = name;
+        emit eventSetGame(gameid, name);
+    }
+
+    function getGame(uint gameid) view public returns (string memory) {
+        return _games[gameid].name;
+    }
+
+	//用户管理: 名称、等级
+	event eventSetMember(uint _memberId, string name, string level);
+
+	struct Member {
+		string name;
+		string level;
+	}
+	mapping(uint => Member) public _members;
+
+	function setMember(uint memberId, string memory name, string memory level) onlyOwner public {
+		Member memory item;
+		item.name = name;
+		item.level = level;
+		_members[memberId] = item;
+		emit eventSetMember(memberId, name, level);
+	}
+
+	function getMember(uint memberId) view public returns (string memory, string memory) {
+		return (_members[memberId].name, _members[memberId].level);
+	}
 
 }
 

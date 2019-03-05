93c5ed009876204a62ab93265ad36b81589f6621
李兴 <289373728@qq.com>
2019-03-05 13:40:09 +0800

    第一次提交，YMChain游米链Token智能合约

---
+++ contracts/Ymchain.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.5.0;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Detailed.sol';
+import "./Ownable.sol";
+
+contract Ymchain is ERC20, ERC20Detailed, Ownable {
+	string private _name = "YMChain";
+    string private _symbol = "YMC";
+    uint8 private _decimals = 18;
+    uint256 private _totalSupply = 2000000000 * (10 ** uint256(_decimals));
+
+
+    constructor(address _owner) Ownable(_owner) public ERC20Detailed(_name, _symbol, _decimals) {
+
+        emit Transfer(address(0), _owner, _totalSupply);
+    }
+
+}
+
---
+++ migrations/2_deploy_contract.js
@@ -0,0 +1,6 @@
+// 2_deploy_contract.js；truffle migrate 命令的执行顺序与文件名有关，所以多个部署脚本需要按照顺序命名
+
+var Ymchain = artifacts.require("Ymchain");
+module.exports = function(deployer) {
+	deployer.deploy(Ymchain);
+};
